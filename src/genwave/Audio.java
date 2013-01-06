f5234bd50b84b5cd98c327740281ab2e9ae20017
MultiTool <JCAtkeson@gmail.com>
2013-01-06 10:09:23 -0500

    first commit

---
+++ src/genwave/Audio.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genwave;
+
+/**
+ *
+ * @author john
+ */
+public class Audio {
+  public void Test(){
+    
+  }
+}
