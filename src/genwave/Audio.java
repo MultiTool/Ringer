4dd50de90eff7596f8809cc9363b6c3138a38c0d
Anonymous <JCAtkeson@gmail.com>
2013-01-12 19:43:34 -0500

    Stripping down audio capture tutorial

--- src/genwave/Audio.java
+++ src/genwave/Audio.java
@@ -4,12 +4,116 @@
  */
 package genwave;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.sound.sampled.*;
+
 /**
  *
- * @author john
+ * @author MultiTool
+ * Cannibalized from 
+ * http://www.java-tips.org/java-se-tips/javax.sound/capturing-audio-with-java-sound-api.html
+ * 
  */
 public class Audio {
-  public void Test(){
-    
+  protected boolean running;
+  ByteArrayOutputStream out;
+  /* **************************************************************************** */
+  public void Capture() {
+    captureAudio();
+    running = false;
+    playAudio();
+  }
+  /* **************************************************************************** */
+  private void captureAudio() {
+    try {
+      final AudioFormat format = getFormat();
+      DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+      final TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
+      line.open(format);
+      line.start();
+      Runnable runner = new Runnable() {
+        int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+        byte buffer[] = new byte[bufferSize];
+        @Override
+        public void run() {
+          out = new ByteArrayOutputStream();
+          running = true;
+          try {
+            while (running) {
+              int count = line.read(buffer, 0, buffer.length);
+              if (count > 0) {
+                out.write(buffer, 0, count);
+              }
+            }
+            out.close();
+          } catch (IOException e) {
+            System.err.println("I/O problems: " + e);
+            System.exit(-1);
+          }
+        }
+      };
+      Thread captureThread = new Thread(runner);
+      captureThread.start();
+    } catch (LineUnavailableException e) {
+      System.err.println("Line unavailable: " + e);
+      System.exit(-2);
+    }
+  }
+  /* **************************************************************************** */
+  private void playAudio() {
+    try {
+      byte audio[] = out.toByteArray();
+      InputStream input = new ByteArrayInputStream(audio);
+      final AudioFormat format = getFormat();
+      final AudioInputStream ais =
+        new AudioInputStream(input, format, audio.length / format.getFrameSize());
+      DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+      final SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+      line.open(format);
+      line.start();
+
+      Runnable runner = new Runnable() {
+        int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+        byte buffer[] = new byte[bufferSize];
+        @Override
+        public void run() {
+          try {
+            int count;
+            while ((count = ais.read(buffer, 0, buffer.length)) != -1) {
+              if (count > 0) {
+                line.write(buffer, 0, count);
+              }
+            }
+            line.drain();
+            line.close();
+          } catch (IOException e) {
+            System.err.println("I/O problems: " + e);
+            System.exit(-3);
+          }
+        }
+      };
+      Thread playThread = new Thread(runner);
+      playThread.start();
+    } catch (LineUnavailableException e) {
+      System.err.println("Line unavailable: " + e);
+      System.exit(-4);
+    }
+  }
+  /* **************************************************************************** */
+  private AudioFormat getFormat() {
+    float sampleRate = 8000;
+    int sampleSizeInBits = 8;
+    int channels = 1;
+    boolean signed = true;
+    boolean bigEndian = true;
+    return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+  }
+  /* **************************************************************************** */
+  public void Test() {
   }
 }
